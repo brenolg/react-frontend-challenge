853bfe6664db1c1851ea662b41e87a031c5a0710
brenolg <brenolgar@gmail.com>
2026-04-10 18:43:53 -0300

    feat: filtro por genero

--- vite-project/src/pages/Home/index.tsx
+++ vite-project/src/pages/Home/index.tsx
@@ -1,14 +1,28 @@
 import { MovieCard } from "@/components/MovieCard";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { useDebounce } from "@/hooks/useDebounce";
-import { getGenres, getPopularMovies, searchMovies } from "@/services/movies";
+import {
+  discoverMovies,
+  getGenres,
+  getPopularMovies,
+  searchMovies,
+} from "@/services/movies";
 import { useMoviesStore } from "@/store/useMoviesStore";
-import type { GenresResponse, Movie, MoviesResponse } from "@/types/movies";
+import type { Movie } from "@/types/movies";
 import { useQueries, useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 
 export default function Home() {
   const { popularMovies, genres } = useMoviesStore();
   const [search, setSearch] = useState("");
+  const [selectedGenre, setSelectedGenre] = useState<number | "all">("all");
   const debouncedSearch = useDebounce(search, 500);
 
   const results = useQueries({
@@ -22,10 +36,7 @@ export default function Home() {
         queryFn: getPopularMovies,
       },
     ],
-  }) as [
-    { data: GenresResponse | undefined; isPending: boolean; error: unknown },
-    { data: MoviesResponse | undefined; isPending: boolean; error: unknown },
-  ];
+  });
 
   const { data: searchData, isFetching: isSearching } = useQuery({
     queryKey: ["search", debouncedSearch],
@@ -33,21 +44,39 @@ export default function Home() {
     enabled: !!debouncedSearch,
   });
 
-  const isLoading = results.some((q) => q.isPending);
+  const { data: discoverData, isFetching: isFiltering } = useQuery({
+    queryKey: ["discover", selectedGenre],
+    queryFn: () =>
+      discoverMovies({
+        genreId: selectedGenre === "all" ? undefined : selectedGenre,
+      }),
+    enabled: selectedGenre !== "all", //
+  });
+
+  const initialLoading = results.some((q) => q.isPending);
   const hasError = results.some((q) => q.error);
+  const isGlobalLoading = initialLoading || isSearching || isFiltering;
 
   if (hasError) return <p>Erro ao carregar</p>;
-  if (isLoading) return <p>Carregando...</p>;
+
   if (!genres || !popularMovies) return null;
 
-  const moviesToShow =
-    debouncedSearch && searchData ? searchData.results : popularMovies;
+  function getMoviesToShow() {
+    if (selectedGenre !== "all" && discoverData) {
+      return discoverData.results;
+    }
+    if (debouncedSearch && searchData) {
+      return searchData.results;
+    }
+    return popularMovies;
+  }
 
+  const moviesToShow = getMoviesToShow();
   console.log(moviesToShow);
 
   return (
     <div className="space-y-4">
-      <input
+      <Input
         type="text"
         placeholder="Buscar filmes..."
         value={search}
@@ -55,7 +84,28 @@ export default function Home() {
         className="w-full p-2 border rounded-md"
       />
 
-      {isSearching && <p>Buscando...</p>}
+      <Select
+        value={selectedGenre === "all" ? "all" : String(selectedGenre)}
+        onValueChange={(value) =>
+          setSelectedGenre(value === "all" ? "all" : Number(value))
+        }
+      >
+        <SelectTrigger className="w-[200px]">
+          <SelectValue placeholder="Selecione um gênero" />
+        </SelectTrigger>
+
+        <SelectContent className="z-50 bg-background">
+          <SelectItem value="all">Todos</SelectItem>
+
+          {genres.map((genre) => (
+            <SelectItem key={genre.id} value={String(genre.id)}>
+              {genre.name}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+
+      {isGlobalLoading && <p>Filtrando...</p>}
 
       <div className="flex flex-wrap gap-4">
         {moviesToShow.map((movie: Movie) => (
--- vite-project/src/services/movies.ts
+++ vite-project/src/services/movies.ts
@@ -1,19 +1,19 @@
 import { useMoviesStore } from "@/store/useMoviesStore";
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL;
-const authorization = `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`;
+export const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
+    "Content-Type": "application/json",
+  },
+  params: {
+    language: "pt-BR",
+  },
+});
 
 export async function getGenres() {
-  const response = await axios.get(`${API_URL}/genre/movie/list`, {
-    headers: {
-      Authorization: authorization,
-      "Content-Type": "application/json",
-    },
-    params: {
-      language: "pt-BR",
-    },
-  });
+  const response = await api.get("/genre/movie/list");
 
   useMoviesStore.getState().setGenres(response.data.genres);
 
@@ -21,44 +21,52 @@ export async function getGenres() {
 }
 
 export async function getPopularMovies() {
-  const response = await fetch(`${API_URL}/movie/popular`, {
-    headers: {
-      Authorization: authorization,
-      "Content-Type": "application/json",
-    },
-  });
+  const response = await api.get("/movie/popular");
 
-  const data = await response.json();
+  useMoviesStore.getState().setPopularMovies(response.data.results);
 
-  useMoviesStore.getState().setPopularMovies(data.results);
-
-  return data;
+  return response.data;
 }
 
 export async function getMovieDetails(id: string) {
-  const res = await fetch(
-    `https://api.themoviedb.org/3/movie/${id}?append_to_response=credits,videos`,
-    {
-      headers: {
-        Authorization: authorization,
-        "Content-Type": "application/json",
-      },
+  const response = await api.get(`/movie/${id}`, {
+    params: {
+      append_to_response: "credits,videos",
     },
-  );
+  });
 
-  return res.json();
+  return response.data;
 }
 
 export async function searchMovies(query: string) {
-  const response = await fetch(
-    `${API_URL}/search/movie?query=${query}&language=pt-BR`,
-    {
-      headers: {
-        Authorization: authorization,
-        "Content-Type": "application/json",
-      },
+  const response = await api.get("/search/movie", {
+    params: {
+      query,
     },
-  );
+  });
 
-  return response.json();
+  return response.data;
+}
+
+export type DiscoverFilters = {
+  genreId?: number;
+  year?: number;
+  minRating?: number;
+};
+
+export async function discoverMovies(filters: DiscoverFilters) {
+  const params = new URLSearchParams();
+
+  if (filters.genreId) params.append("with_genres", String(filters.genreId));
+
+  if (filters.year) params.append("primary_release_year", String(filters.year));
+
+  if (filters.minRating)
+    params.append("vote_average.gte", String(filters.minRating));
+
+  const response = await api.get("/discover/movie", {
+    params,
+  });
+
+  return response.data;
 }
