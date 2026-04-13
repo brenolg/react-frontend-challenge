79a0ed955b09ec7a6dbb5fe81f50d272851c4f4c
brenolg <brenolgar@gmail.com>
2026-04-13 12:08:55 -0300

    chore: testes de login

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -1,13 +1,18 @@
+/// <reference types="vitest" />
 import react from "@vitejs/plugin-react";
 import path from "node:path";
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./src/test/setup.ts",
   },
 });
